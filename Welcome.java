b4022543eb60657735cb32ce0977fd5a5e687e72
vinolinjermiahj <44571711+vinolinjermiahj@users.noreply.github.com>
2018-11-26 13:42:09 +0530

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+package com.sanumala;
+public class HelloWorld
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello gist @ GitHub!");   
+      }
+} 
